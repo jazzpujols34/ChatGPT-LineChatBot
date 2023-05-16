e084d238ac984e9221354a1233c93e2428695f1c
jazzpujols34 <ro5112@hotmail.com>
2023-05-16 15:15:37 +0800

    handler1

--- index.py
+++ index.py
@@ -29,7 +29,7 @@ def aoai(q):
     return msg
 
 line_bot_api = LineBotApi(os.getenv('LINE_ACCESS_TOKEN'))
-handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
+handler1 = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
 @app.route("/")
 def saturday():
@@ -41,13 +41,13 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     try:
-        handler.handle(body, signature)
+        handler1.handle(body, signature)
     except InvalidSignatureError:
         print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler1.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
